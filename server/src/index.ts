452658e0a66c9444e69393807dea7f0b7f51f1e1
chaelin1211 <cofls749@naver.com>
2024-12-13 17:10:13 +0900

    FEAT: POST Body Data 파싱을 위한 middleware 설정 추가

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,13 @@ app.listen(port, () => {
   console.log(`[server]: Server is running at https://localhost:${port}`);
 });
 
+// Middleware
+// body parser
+app.use(express.json());
+app.use(express.urlencoded({extended: true})); // 내장 queryString module 사용
+
 // Route
 app.use('/sample', sampleRouter);
+
 
 export default app;

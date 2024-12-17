133ca40c8607fd7d05fb52dfb094f67ef4cde6c6
chaelin1211 <cofls749@naver.com>
2024-12-17 14:18:11 +0900

    FEAT: 목록 조회 api, 수정 기능 추가

--- server/src/controllers/yarnController.ts
+++ server/src/controllers/yarnController.ts
@@ -15,14 +15,34 @@ export async function getYarn(req, res) {
   }
 }
 
-export async function setYarns(req, res) {
+export async function getYarns(req, res) {
+  const db = await connectToDatabase();
+  const collection = db.collection("yarn");
+
+  const cursor = await collection.find({})
+    ?.skip(Number.parseInt(req.query.from) || 0)
+    ?.limit(Number.parseInt(req.query.size) || 20)?.toArray();
+  if (cursor) {
+    res.status(200).send(cursor);
+  } else {
+    res.status(404).send('No matching document found');
+  }
+}
+
+export async function saveYarn(req, res) {
   const db = await connectToDatabase();
   const yarn: Yarn = req.body;
-
   const collection = db.collection("yarn");
-  const result = await collection.insertOne(yarn);
+
+  let result;
+  if(!!yarn.id) {
+    result = await collection.findOneAndUpdate({_id: new ObjectId(yarn.id)}, {$set: yarn});
+  } else {
+    result = await collection.insertOne(yarn);
+  }
+
   res.status(201).json({
     success: true,
-    data: {id: result.insertedId}
+    data: result
   });
 }
--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import {getYarn, setYarns} from "../controllers/yarnController.js";
+import {getYarn, getYarns, saveYarn} from "../controllers/yarnController.js";
 
 const router = express.Router();
 
 const YARN_PATH = '/yarn';
 router.get(YARN_PATH, getYarn);
-router.post(YARN_PATH, setYarns);
+router.get(YARN_PATH+'/list', getYarns);
+router.post(YARN_PATH, saveYarn);
 
 export default router;
